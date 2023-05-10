b2ade1ec9984a2866c7120e19a6168d895e25853
energy1 <webdizdev@gmail.com>
2023-05-10 14:07:44 +0300

    linter format; renaming

--- form_handler.js
+++ form_handler.js
@@ -1,37 +1,39 @@
-const inputDate1 = document.getElementById('inputDate1');
-inputDate1.addEventListener('input', dateEventMask);
+const inputDatePersonal = document.getElementById("inputDatePersonal");
+inputDatePersonal.addEventListener("input", dateEventMask);
 
-const inputDate2 = document.getElementById('inputDate2');
-inputDate2.addEventListener('input', dateEventMask);
+const inputDatePartner1 = document.getElementById("inputDatePartner1");
+inputDatePartner1.addEventListener("input", dateEventMask);
+
+const inputDatePartner2 = document.getElementById("inputDatePartner2");
+inputDatePartner2.addEventListener("input", dateEventMask);
 
 function calcPersonal() {
-	inputDate2.value = "";
-	calculate();
+    // inputDate2.value = "";
+    calculate(inputDatePersonal.value);
 }
-function calc_2() {
-	calculate();
+function calc_compatibility() {
+    calculate(inputDatePartner1.value, inputDatePartner2.value);
 }
 
 function dateEventMask(e) {
-	var val = e.data;
-	var str = e.target.value;
-	var len = e.target.value.length;
-	
-	/**remove unallowed characters */
-	if(val && !val.match(/[0-9.]/)) 
-	{
-		e.target.value = str.substring(0, str.length - 1);			
-	}
+    var val = e.data;
+    var str = e.target.value;
+    var len = e.target.value.length;
 
-	if(len !== 1 || len !== 3) {
-		if(val == '.') {
-			e.target.value = str.substring(0, str.length - 1);			
-		}
-	}
-	if(len === 2) {
-		e.target.value += '.';
-	}
-	if(len === 5) {
-		e.target.value += '.';
-	}		
-};
+    /**remove unallowed characters */
+    if (val && !val.match(/[0-9.]/)) {
+        e.target.value = str.substring(0, str.length - 1);
+    }
+
+    if (len !== 1 || len !== 3) {
+        if (val == ".") {
+            e.target.value = str.substring(0, str.length - 1);
+        }
+    }
+    if (len === 2) {
+        e.target.value += ".";
+    }
+    if (len === 5) {
+        e.target.value += ".";
+    }
+}
